07a007482868c3c90ebd6a9987fd61e0e86a6838
Eudesx <eudesvicente@rocketmail.com>
2023-10-22 22:52:54 -0300

    Função

---
+++ funções.js
@@ -0,0 +1,23 @@
+torrar()
+saudar()
+
+function torrar(){
+    console.log("Torrando, pão!");
+    injetarPao()
+    fazerMagica()
+    
+}
+function injetarPao(){
+    console.log("Preparando par injetar pão");
+    console.log("Finalizado");
+}
+
+function fazerMagica(){
+    console.log("Abracadabra! Coisas Mágicas aconteceram!");
+}
+function saudar(nome){
+    console.log("Olá," + nome + "!Como voçê esta?");
+}
+    saudar("Pedrinho");
+    saudar("Mariana");
+
