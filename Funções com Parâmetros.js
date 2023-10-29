a982abaf12f2f0bec20b7b9a4faac1f2fc8c033f
Eudesx <eudesvicente@rocketmail.com>
2023-10-28 21:14:30 -0300

    Funções com Parametros

---
+++ Funções com Parâmetros.js
@@ -0,0 +1,15 @@
+/*dizerOla();
+function dizerOla(nome = "Pedrinho", outroNome = "Mariana"){
+
+console.log("Olá, " + nome + " !Como voçê esta?");
+
+}*/
+
+
+function calcularIdade(nome, anoNascimento){
+    let idade = 2023 - anoNascimento;
+    console.log(nome + " tem " + idade + " anos!");
+}
+calcularIdade("Pedrinho", 2010);
+calcularIdade("Mariana", 2008);
+
