da177b82a7b2374f4645a6d1cad96607de4f4f6e
Eudesx <eudesvicente@rocketmail.com>
2023-11-03 21:54:37 -0300

    Atualização

--- Explorando Condicionais com JavaScript.js
+++ Explorando Condicionais com JavaScript.js
@@ -1,22 +1,16 @@
-//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
-//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
-//- "print": imprime um texto de saída (output), pulando linha.
-
-// Entrada de dados. Lembre-se: O parseInt(()) é importante para a conversão dos valores de entrada(String) para um valor numérico(int).
 let escolhaDoTreinador = parseInt(gets());
 let pokemonEscolhido; 
 
-//TODO: Implemente as condições necessárias para a solução do desafio. Utilize a tabela de exemplos para identificar a escolha do treinador:
-if ( escolhaDoTreinador === 1) {
+// Implementação das condições necessárias para a solução do desafio
+if (escolhaDoTreinador === 1) {
     pokemonEscolhido = "Bulbasaur";
-} else if ( escolhaDoTreinador ===2  ) {
+} else if (escolhaDoTreinador === 2) {
     pokemonEscolhido = "Charmander";
-} else if ( escolhaDoTreinador ===3 ) {
+} else if (escolhaDoTreinador === 4) {
     pokemonEscolhido = "Pikachu";
-} else{
+} else {
     pokemonEscolhido = "Mewtwo";
 }
 
-//Imprime o Pokémon escolhido:
-if (pokemonEscolhido) {
-    print("Voce escolheu o " + pokemonEscolhido + " como seu Pokemon inicial.");
+// Imprime o Pokémon escolhido
+print("Voce escolheu o " + pokemonEscolhido + " como seu Pokemon inicial.");
